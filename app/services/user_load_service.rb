73a4d5f646e149735a8fe9336517f5e381eed9ee
Guy Chamberlain <guy.821020312562@gmail.com>
2015-11-01 17:54:05 +1100

    fixed csv reading

--- app/services/user_load_service.rb
+++ app/services/user_load_service.rb
@@ -7,13 +7,13 @@ class UserLoadService
 
 	  	@file = file
 	 	CSV.foreach(@file.path, headers: true) do |row|
-	 		r = row.to_hash 		
-	 		user = User.create 	email_address: r['email_address'],
-		 	 					first_name: r['first_name'],
-		 	 					last_name: r['last_name']	
+	 		
+	 		user = User.create 	email_address: row['email_address'],
+		 	 					first_name: row['first_name'],
+		 	 					last_name: row['last_name']	
 		 	
-		 	if r['reporting_to_email_address'].present? 
-		 		add_boss user, r['reporting_to_email_address']
+		 	if row['reporting_to_email_address'].present? 
+		 		add_boss user, row['reporting_to_email_address']
 		 	end
 	 	end 
 
@@ -31,19 +31,19 @@ class UserLoadService
 		 	else
 		 		# Check CSV file for boss
 		 		CSV.foreach(@file.path, headers: true) do |row|
-	 				r = row.to_hash 		
-	 				if r['email_address'] == reporting_to_email_address
-	 					boss = User.create 	email_address: r['email_address'],
-		 	 								first_name: r['first_name'],
-		 	 								last_name: r['last_name']	
+	 					
+	 				if row['email_address'] == reporting_to_email_address
+	 					boss = User.create 	email_address: row['email_address'],
+		 	 								first_name: row['first_name'],
+		 	 								last_name: row['last_name']	
 						
 						# Setup the relationship to this boss
 						created_worker.reports_to_user = boss
 		 				created_worker.save
 
 		 				# Check if the boss has a boss and recurse
-						if r['reporting_to_email_address'].present? 
-		 						add_boss boss, r['reporting_to_email_address']
+						if row['reporting_to_email_address'].present? 
+		 						add_boss boss, row['reporting_to_email_address']
 		 				end
 		 			end
 		 		end	
